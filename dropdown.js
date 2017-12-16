82d91ed8a4e48b0f78a40a0c2c2f446a6efa5852
Clare <chblackburne@gmail.com>
2017-12-16 12:46:14 +0000

    modify dropdown effect

--- dropdown.js
+++ dropdown.js
@@ -10,18 +10,3 @@ var dropdowncontent = document.getElementById("myDropdown");
   dropdowncontent.addEventListener("mouseout", function(){
   document.getElementById("myDropdown").classList.toggle("show");
 });
-
-//closes when user clicks somewhere else
-// window.onclick = function(event) {
-//   if (!event.target.matches('.dropdownbtn')) {
-//
-//     var dropdowns = document.getElementsByClassName("dropdown-content");
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//       }
-//     }
-//   }
-// }

55c221095e2036e7128f48f2c6d5c315c5bf95bf
Clare <chblackburne@gmail.com>
2017-12-16 12:10:21 +0000

    refactor menu to fit account

--- dropdown.js
+++ dropdown.js
@@ -1,22 +1,27 @@
 
-function myFunction() {
+
+var drop = document.getElementById("accountbtn");
+drop.addEventListener("mouseover", function(){
   //toggle is really nifty - we could use this for the list functionality as well
     document.getElementById("myDropdown").classList.toggle("show");
+});
 
-    // document.getElementById("myDropdown").classList
-}
+var dropdowncontent = document.getElementById("myDropdown");
+  dropdowncontent.addEventListener("mouseout", function(){
+  document.getElementById("myDropdown").classList.toggle("show");
+});
 
 //closes when user clicks somewhere else
-window.onclick = function(event) {
-  if (!event.target.matches('.dropdownbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropdownbtn')) {
+//
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }

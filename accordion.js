912cb55ea44c54510ace40751e518a907ea28084
Clare <chblackburne@gmail.com>
2017-12-16 19:49:51 +0000

    autoscroll to show bottom panel

--- accordion.js
+++ accordion.js
@@ -1,10 +1,14 @@
 var acc = document.getElementsByClassName("header");
 var i;
+var shown = document.querySelector('p')
 
 for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
+    acc[i].addEventListener("click", function(s) {
       var content  = this.nextElementSibling;
       console.log("hello");
       content.classList.toggle("panel");
-    });
-};
+      var rect = shown.getBoundingClientRect();
+      console.log(rect.top, rect.right, rect.bottom, rect.left);
+      window.scrollTo(0, rect.right);
+});
+}
